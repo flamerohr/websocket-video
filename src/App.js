96fd9f00f64e815ed8113ff2beb9106682e7eebb
Chris Joe <chris.joe@soulmachines.com>
2021-03-11 22:12:00 +1300

    feat: add video element with rough layout to start with

--- src/App.js
+++ src/App.js
@@ -4,8 +4,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Insert app here :)
+        React to video in React.
       </header>
+      <div className="App-container">
+        <video className="App-video" controls src="/Big_Buck_Bunny_1080_10s_5MB.mp4" />
+        <div className="App-sidebar">
+
+        </div>
+      </div>
     </div>
   );
 }
