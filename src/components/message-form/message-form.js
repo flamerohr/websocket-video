0bf999501a2417305a8b6e16da3ca504df08e904
Chris Joe <chris.joe@soulmachines.com>
2021-03-12 22:00:03 +1300

    implement submit function for the form

--- src/components/message-form/message-form.js
+++ src/components/message-form/message-form.js
@@ -1,14 +1,35 @@
+import { useCallback } from 'react';
 import './message-form.css';
 
+// todo: change to a controlled form with form library
 /**
  * A form component for handling message data
  * @param {functioin} onSubmit the callback for when the form is submitting values
  */
 export const MessageForm = ({ onSubmit }) => {
+  const submitData = useCallback((e) => {
+    e.preventDefault();
 
-  // todo: change to a controlled form with form library
+    const elements = e.target.elements;
+    let data = {};
+
+    for (let i = 0; i < elements.length; i++) {
+      const element = elements[i];
+
+      if (!element.name) {
+        continue;
+      }
+      data = {
+        ...data,
+        [element.name]: element.value,
+      };
+    }
+
+    onSubmit(data);
+  }, [onSubmit]);
+
   return (
-    <form onSubmit={(e) => console.log(e)}>
+    <form onSubmit={submitData}>
       <label className="App-label">
         <span className="App-label-text">Message</span>
         <input name="text" />
