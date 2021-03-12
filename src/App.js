caecd44737d838ab31d88c7a79de1b0f1c7d2f6d
Chris Joe <chris.joe@soulmachines.com>
2021-03-12 23:07:53 +1300

    capture current time from video element

--- src/App.js
+++ src/App.js
@@ -2,19 +2,22 @@ import './App.css';
 import Sidebar from './components/sidebar';
 import Video from './components/video/video';
 import { MessageListProvider } from './hooks/use-message-list';
+import { VideoProvider } from './hooks/use-video';
 
 function App() {
   return (
     <MessageListProvider>
-      <div className="App">
-        <header className="App-header">
-          React to video in React.
-        </header>
-        <div className="App-container">
-          <Video />
-          <Sidebar />
+      <VideoProvider>
+        <div className="App">
+          <header className="App-header">
+            React to video in React.
+          </header>
+          <div className="App-container">
+            <Video />
+            <Sidebar />
+          </div>
         </div>
-      </div>
+      </VideoProvider>
     </MessageListProvider>
   );
 }
--- src/components/video/video.js
+++ src/components/video/video.js
@@ -1,11 +1,26 @@
+import { useEffect, useRef } from 'react';
+import { useVideoActions } from '../../hooks/use-video';
 import './video.css';
 
 /**
  * For handling video related events and functions
  */
 export const Video = () => {
+  const videoRef = useRef();
+  const { setTime } = useVideoActions();
+
+  useEffect(() => {
+    const video = videoRef.current;
+    const callback = () => setTime(video.currentTime);
+    video.addEventListener('timeupdate', callback);
+
+    return () => {
+      video.removeEventListener('timeupdate', callback);
+    }
+  }, [setTime]);
+
   return (
-    <video className="App-video" controls src="/Big_Buck_Bunny_1080_10s_5MB.mp4" />
+    <video className="App-video" ref={videoRef} controls src="/Big_Buck_Bunny_1080_10s_5MB.mp4" />
   );
 };
 
