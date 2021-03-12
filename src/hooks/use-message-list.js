7e559a1ba60d0728f4429834fc1bf6c7720ccb0f
Chris Joe <chris.joe@soulmachines.com>
2021-03-12 22:06:45 +1300

    create a message list state context

---
+++ src/hooks/use-message-list.js
@@ -0,0 +1,38 @@
+import { createContext, useContext, useMemo, useState } from 'react';
+
+// share state and actions separately
+const MessageListStateContext = createContext();
+const MessageListActionsContext = createContext();
+
+/**
+ * Provider for a message list state
+ * @param {messages[]} initialValue An array of messages to start with
+ */
+export const MessageListProvider = ({ children, initialValue = [] }) => {
+  const [messages, setMessages] = useState(initialValue);
+
+  const actions = useMemo(() => ({
+    addMessage: (message) => (
+      setMessages([
+        ...messages,
+        message,
+      ])
+    ),
+  }), [messages, setMessages]);
+
+  return (
+    <MessageListStateContext.Provider value={messages}>
+      <MessageListActionsContext.Provider value={actions}>
+        {children}
+      </MessageListActionsContext.Provider>
+    </MessageListStateContext.Provider>
+  );
+};
+
+export const useMessageListState = () => {
+  return useContext(MessageListStateContext);
+};
+
+export const useMessageListActions = () => {
+  return useContext(MessageListActionsContext);
+};
