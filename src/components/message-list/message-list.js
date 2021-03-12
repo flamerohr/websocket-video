d5eafd6ce2d5f50d8aecd9fa236ba4f5b25f45e7
Chris Joe <chris.joe@soulmachines.com>
2021-03-12 23:08:36 +1300

    filter out messages that do not have the correct timestamp

--- src/components/message-list/message-list.js
+++ src/components/message-list/message-list.js
@@ -1,14 +1,27 @@
+import { useMemo } from "react";
 import { useMessageListState } from "../../hooks/use-message-list";
+import { useVideoState } from "../../hooks/use-video";
 import Message from './message';
 
 export const MessageList = () => {
   const messages = useMessageListState();
+  const { time } = useVideoState();
+
+  const filteredMessages = useMemo(() => (
+    messages
+      .filter(({ timestamp, duration }) => (
+        time > timestamp && time < (timestamp + duration)
+      ))
+  ), [messages, time]);
 
   return (
     <div>
       <h2>Messages</h2>
       <div>
-        {messages.map((message) => <Message key={message.id} {...message} />)}
+        {
+        filteredMessages
+          .map((message) => <Message key={message.id} {...message} />)
+        }
       </div>
     </div>
   )
